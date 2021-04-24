e4ef7361fb25ed9b478c111ca1934bfc02923a46
JaiRaga <raga.j.santhosh@gmail.com>
2021-04-24 11:41:02 +0530

    createed mongodb schema

--- models/User.js
+++ models/User.js
@@ -1,84 +1,91 @@
-const mongoose = require("mongoose");
-const validator = require("validator");
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
-const tokenSecret = require("../config/keys").tokenSecret;
-const Schema = mongoose.Schema;
+const mongoose = require('mongoose')
+const validator = require('validator')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const tokenSecret = require('../config/keys').tokenSecret
+const Schema = mongoose.Schema
 
 const userSchema = new Schema(
-  {
-    handle: {
-      type: String,
-      required: true
-    },
-    avatar: {
-      type: String
-    },
-    username: {
-      type: String,
-      required: true
-    },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-      lowercase: true,
-      validate(value) {
-        if (!validator.isEmail(value)) {
-          throw new Error("Not a valid Email address!");
-        }
-      }
-    },
-    password: {
-      type: String,
-      required: true,
-      trim: true,
-      minlength: 6,
-      validate(value) {
-        const val = value.toLowerCase();
-        if (val.includes("password")) {
-          throw new Error("Cannot contain the word 'password'");
-        }
-      }
-    },
-    followers: [
-      {
-        userId: {
-          type: Schema.Types.ObjectId,
-          required: true
-        }
-      }
-    ],
-    following: [
-      {
-        userId: {
-          type: Schema.Types.ObjectId,
-          required: true
-        }
-      }
-    ],
-    caption: {
-      type: String
-    },
-    tokens: [
-      {
-        token: {
-          type: String,
-          required: true
-        }
-      }
-    ]
-  },
-  {
-    timestamps: true
-  }
-);
+	{
+		name: {
+			type: String,
+			required: true,
+		},
+		avatar: { type: String },
+		profilelink: {
+			type: String,
+			required: true,
+		},
+		location: {
+			type: String,
+			required: true,
+		},
+		education: { type: String },
+		challengesSolved: { type: Number },
+		solutionsSubmitted: { type: Number },
+		solutionAccepted: { type: Number },
+		overallRank: { type: Number },
+		numberOfVotes: { type: Number },
+		deviceType: { type: String },
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+			lowercase: true,
+			validate(value) {
+				if (!validator.isEmail(value)) {
+					throw new Error('Not a valid Email address!')
+				}
+			},
+		},
+		password: {
+			type: String,
+			required: true,
+			trim: true,
+			minlength: 6,
+			validate(value) {
+				const val = value.toLowerCase()
+				if (val.includes('password')) {
+					throw new Error("Cannot contain the word 'password'")
+				}
+			},
+		},
+		followers: [
+			{
+				userId: {
+					type: Schema.Types.ObjectId,
+					required: true,
+				},
+			},
+		],
+		following: [
+			{
+				userId: {
+					type: Schema.Types.ObjectId,
+					required: true,
+				},
+			},
+		],
+		caption: { type: String },
+		tokens: [
+			{
+				token: {
+					type: String,
+					required: true,
+				},
+			},
+		],
+	},
+	{
+		timestamps: true,
+	}
+)
 
-userSchema.virtual("tweets", {
-  ref: "Tweet",
-  localField: "_id",
-  foreignField: "owner"
-});
+userSchema.virtual('tweets', {
+	ref: 'Tweet',
+	localField: '_id',
+	foreignField: 'owner',
+})
 
 // userSchema.virtual("comments", {
 //   ref: "Tweet",
@@ -87,80 +94,80 @@ userSchema.virtual("tweets", {
 // });
 
 userSchema.methods.toJSON = function () {
-  const user = this;
-  const userObject = user.toObject();
+	const user = this
+	const userObject = user.toObject()
 
-  delete userObject.password;
-  delete userObject.tokens;
+	delete userObject.password
+	delete userObject.tokens
 
-  return userObject;
-};
+	return userObject
+}
 
 userSchema.methods.generateAuthToken = async function () {
-  const user = this;
-  const token = jwt.sign({ _id: user._id.toString() }, tokenSecret, {
-    expiresIn: "7 days"
-  });
+	const user = this
+	const token = jwt.sign({ _id: user._id.toString() }, tokenSecret, {
+		expiresIn: '7 days',
+	})
 
-  user.tokens = user.tokens.concat({ token });
-  await user.save();
-  return token;
-};
+	user.tokens = user.tokens.concat({ token })
+	await user.save()
+	return token
+}
 
 userSchema.methods.updateUser = async function (
-  username,
-  handle,
-  email,
-  avatar
+	username,
+	handle,
+	email,
+	avatar
 ) {
-  let user = this;
+	let user = this
 
-  username ? (user.username = username) : null;
-  handle ? (user.handle = handle) : null;
-  email ? (user.email = email) : null;
-  avatar ? (user.avatar = avatar) : null;
+	username ? (user.username = username) : null
+	handle ? (user.handle = handle) : null
+	email ? (user.email = email) : null
+	avatar ? (user.avatar = avatar) : null
 
-  console.log("Avatar....", user.avatar, avatar, username, email, handle);
-  await user.save();
-  console.log("Avatar....", user.avatar, avatar);
+	console.log('Avatar....', user.avatar, avatar, username, email, handle)
+	await user.save()
+	console.log('Avatar....', user.avatar, avatar)
 
-  return user;
-};
+	return user
+}
 
 userSchema.statics.findByCredentials = async (email, password) => {
-  const user = await User.findOne({ email });
+	const user = await User.findOne({ email })
 
-  if (!user) {
-    console.log("FIRST");
-    throw new Error("Unable to login");
-  }
+	if (!user) {
+		console.log('FIRST')
+		throw new Error('Unable to login')
+	}
 
-  const isMatch = await bcrypt.compare(password, user.password);
+	const isMatch = await bcrypt.compare(password, user.password)
 
-  if (!isMatch) {
-    console.log("SECOND");
-    throw new Error("Unable to login");
-  }
+	if (!isMatch) {
+		console.log('SECOND')
+		throw new Error('Unable to login')
+	}
 
-  return user;
-};
+	return user
+}
 
-userSchema.pre("save", async function (next) {
-  const user = this;
+userSchema.pre('save', async function (next) {
+	const user = this
 
-  if (user.isModified("password")) {
-    user.password = await bcrypt.hash(user.password, 8);
-  }
+	if (user.isModified('password')) {
+		user.password = await bcrypt.hash(user.password, 8)
+	}
 
-  next();
-});
+	next()
+})
 
-userSchema.pre("remove", async function (next) {
-  const user = this;
-  await Tweet.deleteMany({ owner: user._id });
-  next();
-});
+userSchema.pre('remove', async function (next) {
+	const user = this
+	await Tweet.deleteMany({ owner: user._id })
+	next()
+})
 
-const User = mongoose.model("User", userSchema);
+const User = mongoose.model('User', userSchema)
 
-module.exports = User;
+module.exports = User
